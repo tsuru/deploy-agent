d3714da73f90323a6ed37cd39ba4e94c7ac9efbe
Tarsis Azevedo <tarsis.azevedo@gmail.com>
2015-09-14 18:40:42 -0300

    add test suite.

---
+++ suite_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/tsuru/tsuru/exec/exectest"
+	"github.com/tsuru/tsuru/fs/fstest"
+	"gopkg.in/check.v1"
+	"testing"
+)
+
+func Test(t *testing.T) { check.TestingT(t) }
+
+type S struct {
+	fs   *fstest.RecordingFs
+	exec *exectest.FakeExecutor
+}
+
+var _ = check.Suite(&S{})
+
+func (s *S) SetUpTest(c *check.C) {
+	s.fs = &fstest.RecordingFs{}
+	fsystem = s.fs
+	s.exec = &exectest.FakeExecutor{}
+	osExecutor = s.exec
+	err := s.fs.Mkdir(workingDir, 0777)
+	c.Assert(err, check.IsNil)
+}
+
+func (s *S) TeardownTest(c *check.C) {
+	fsystem = nil
+}
--- tasks_test.go
+++ tasks_test.go
@@ -4,27 +4,7 @@ import (
 	"fmt"
 	"github.com/tsuru/tsuru/fs/fstest"
 	"gopkg.in/check.v1"
-	"testing"
 )
-
-func Test(t *testing.T) { check.TestingT(t) }
-
-type S struct {
-	fs *fstest.RecordingFs
-}
-
-var _ = check.Suite(&S{})
-
-func (s *S) SetUpTest(c *check.C) {
-	s.fs = &fstest.RecordingFs{}
-	fsystem = s.fs
-	err := s.fs.Mkdir(workingDir, 0777)
-	c.Assert(err, check.IsNil)
-}
-
-func (s *S) TeardownTest(c *check.C) {
-	fsystem = nil
-}
 
 func (s *S) TestExecScript(c *check.C) {
 	oldWorkingDir := workingDir
