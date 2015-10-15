6f761650e157e7ce62f04930fe84c8eede75c8f2
Francisco Souza <fss@corp.globo.com>
2015-10-15 18:50:49 -0300

    main: add flag for printing current version
    Also bumping to version 0.1.9.

--- main.go
+++ main.go
@@ -5,9 +5,24 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 )
 
+const version = "0.1.9"
+
+var printVersion bool
+
+func init() {
+	flag.BoolVar(&printVersion, "version", false, "Print version and exit")
+	flag.Parse()
+}
+
 func main() {
+	if printVersion {
+		fmt.Printf("deploy-agent version %s\n", version)
+		return
+	}
 	deployAgent(os.Args[1:])
 }
