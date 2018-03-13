85d00a682986700ea2bd77d403b1f7fbb82a3670
André Carvalho <asantostc@gmail.com>
2018-03-13 18:54:56 -0300

    docker: use docker API for executor

--- internal/docker/executor.go
+++ internal/docker/executor.go
@@ -5,23 +5,50 @@
 package docker
 
 import (
+	"fmt"
+
+	"github.com/fsouza/go-dockerclient"
 	"github.com/tsuru/tsuru/exec"
 )
 
-const dockerBinary = "/usr/local/bin/docker"
-
 // Executor uses docker exec to execute a command in a running docker container
 type Executor struct {
-	containerID string
-	executor    exec.Executor
+	ContainerID string
+	Client      *Client
 }
 
 func (d *Executor) Execute(opts exec.ExecuteOptions) error {
-	// TODO: use the exec API to properly configure workdir etc...
-	if d.executor == nil {
-		d.executor = &exec.OsExecutor{}
+	cmd := append([]string{opts.Cmd}, opts.Args...)
+	if opts.Dir != "" {
+		cmd = append([]string{"cd", opts.Dir, "&&"}, cmd...)
 	}
-	opts.Args = append([]string{"exec", "-t", d.containerID, opts.Cmd}, opts.Args...)
-	opts.Cmd = dockerBinary
-	return d.executor.Execute(opts)
+	// TODO: Envs for API pre v1.25
+	e, err := d.Client.api.CreateExec(docker.CreateExecOptions{
+		Container:    d.ContainerID,
+		Cmd:          cmd,
+		AttachStdin:  opts.Stdin != nil,
+		AttachStdout: opts.Stdout != nil,
+		AttachStderr: opts.Stderr != nil,
+	})
+	if err != nil {
+		return err
+	}
+	err = d.Client.api.StartExec(e.ID, docker.StartExecOptions{
+		OutputStream: opts.Stdout,
+		InputStream:  opts.Stdin,
+		ErrorStream:  opts.Stderr,
+		Tty:          true,
+		RawTerminal:  true,
+	})
+	if err != nil {
+		return err
+	}
+	execData, err := d.Client.api.InspectExec(e.ID)
+	if err != nil {
+		return err
+	}
+	if execData.ExitCode != 0 {
+		return fmt.Errorf("container exited with error: %v", execData.ExitCode)
+	}
+	return nil
 }
