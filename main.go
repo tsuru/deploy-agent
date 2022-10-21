da02f09e762801b2068906a20a76014d937e57d0
Claudio Netto <nettinhorama@gmail.com>
2022-10-21 19:16:36 -0300

    creating gRPC server

---
+++ main.go
@@ -0,0 +1,54 @@
+// Copyright 2022 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"google.golang.org/grpc"
+)
+
+var cfg struct {
+	Port int
+}
+
+func main() {
+	flag.IntVar(&cfg.Port, "port", 4444, "Server TCP port")
+	flag.Parse()
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to listen: %v", err)
+		os.Exit(1)
+	}
+
+	s := grpc.NewServer()
+	go handleGracefulTermination(s)
+
+	fmt.Println("Starting gRPC server at", l.Addr().String())
+
+	if err := s.Serve(l); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to run gRPC server:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("gRPC server terminated")
+}
+
+func handleGracefulTermination(s *grpc.Server) {
+	defer func() {
+		fmt.Fprintln(os.Stdout, "Received termination signal. Terminating gRPC server...")
+		s.GracefulStop()
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+}
